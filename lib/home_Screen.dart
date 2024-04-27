087c5b33216db4f377780439ef9264636a62e219
ahmed sakr <asakr5728@gmail.com>
2024-04-27 15:07:51 +0300

    add home screen

---
+++ lib/home_Screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Home_Screen extends StatelessWidget {
+  const Home_Screen({super.key});
+  static const String routName = "home";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("home"),
+      ),
+    );
+  }
+}
